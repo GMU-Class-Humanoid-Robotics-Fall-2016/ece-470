87f980a1f0a3021bf99d24549d07d7ba3b3335f2
ece-470 <rahmed9@masonlive.gmu.edu>
2016-09-06 00:55:20 -0400

    Create EndEffectorPoint.py

---
+++ EndEffectorPoint.py
@@ -0,0 +1,12 @@
+import math
+
+def endPoint(degree1, degree2, degree3):
+    degree1 = degree1 * (math.pi / 180)
+    degree2 = degree2 * (math.pi / 180)
+    degree3 = degree3 * (math.pi / 180)
+    x = 1*math.cos(degree1)+1*math.cos(degree1+degree2)+1*math.cos(degree1+degree2+degree3)
+    y = 1*math.sin(degree1)+1*math.sin(degree1+degree2)+1*math.sin(degree1+degree2+degree3)
+    print "x = ", x
+    print "y = ", y
+
+endPoint(45,315,45)
