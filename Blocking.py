b6217d586872c33d28240e40ab9063f2db5d9091
ece-470 <rahmed9@masonlive.gmu.edu>
2016-09-13 10:30:36 -0400

    Update Blocking.py

--- Blocking.py
+++ Blocking.py
@@ -1,12 +1,14 @@
 import time
 
+def foo():
+	time.sleep(0.20)
+
 def main():
 	while(True):
 		t0 = time.clock()
 		t1 = time.clock()
 		dt = t1 - t0
-		check = .20 - dt
-		while(0 < (.20 - dt)):
+		while(0 < (1 - dt)):
 			dt = time.clock() - t0
 		print dt
 
