62d676508efcb8d19b2c9a4acf4edd0bbfb0f2eb
ece-470 <rahmed9@masonlive.gmu.edu>
2016-09-13 11:39:08 -0400

    Update Loop-Interrupt.py

--- Loop-Interrupt.py
+++ Loop-Interrupt.py
@@ -1,1 +1,17 @@
-Loop
+import time
+
+def foo():
+	time.sleep(0.20)
+
+def main():
+	while(True):
+		time.sleep(1)
+		while(True):
+			t0 = time.time()
+			foo()
+			t1 = time.time()
+			dt = t1 - t0
+			break
+		print dt
+
+main()
