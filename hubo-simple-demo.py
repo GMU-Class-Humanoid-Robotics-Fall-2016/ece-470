d8d28484830d50564d9b5158fb85943833591ee2
ece-470 <rahmed9@masonlive.gmu.edu>
2016-09-20 15:41:25 -0400

    Update hubo-simple-demo.py

--- hubo-simple-demo.py
+++ hubo-simple-demo.py
@@ -72,13 +72,13 @@ for x in positionBend:
 
 	# Write to the feed-forward channel
 	r.put(ref)
-	simSleep(0.2)
+	simSleep(0.02)
 
 simSleep(0.2)
 
 for i in range(3):
 	print "tilt"	
-	positionTilt = [0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.07, 0.09, 0.1]
+	positionTilt = [0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.07, 0.09, 0.1, 0.12]
 	for x in positionTilt:
 		ref.ref[ha.RHR] = x
 		ref.ref[ha.RAR] = -x
@@ -92,23 +92,26 @@ for i in range(3):
 	simSleep(0.2)
 
 	print "lift foot"
-	positionLiftFoot = .3
-	ref.ref[ha.LHP] = -positionLiftFoot
-	ref.ref[ha.LKN] = 2*positionLiftFoot
-	ref.ref[ha.LAP] = -positionLiftFoot
+	positionLiftFoot = [0.31, 0.32, 0.33]
+	for x in positionLiftFoot:
+		ref.ref[ha.LHP] = -x
+		ref.ref[ha.LKN] = 2*x 
+		ref.ref[ha.LAP] = -x 
+	
+		# Write to the feed-forward channel
+		r.put(ref)
+		simSleep(0.2)
 
-	# Write to the feed-forward channel
-	r.put(ref)
-	simSleep(0.2)
+	simSleep(1)
 
 	print "extend foot"
 	positionExtendFoot = .2
 	ref.ref[ha.LKN] = 2*positionExtendFoot
-	ref.ref[ha.LAP] = -(positionExtendFoot - positionExtendFoot/2)
+	ref.ref[ha.LAP] = -(positionExtendFoot - 0.1)
 
 	# Write to the feed-forward channel
 	r.put(ref)
-	simSleep(0.2)
+	simSleep(2)
 
 	print "tilt"
 	positionTilt = [0.1, 0.09, 0.07, 0.05, 0.04, 0.03, 0.02, 0.01, 0]
